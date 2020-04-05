d6a0d282f36426796c53087fb684d8c00a75982e
Absolutely <absolutely1756@gmail.com>
2020-04-05 12:23:39 +0700

    add chat

---
+++ src/main/java/com/apit/tictactoe/controller/ChatController.java
@@ -0,0 +1,18 @@
+package com.apit.tictactoe.controller;
+
+import com.apit.tictactoe.chat.SendMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    private final SimpMessagingTemplate simpMessagingTemplate;
+    public ChatController(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+    @MessageMapping("/message")
+    public void getMessages(SendMessage request){
+        this.simpMessagingTemplate.convertAndSend("/chat/message/" + request.getIdChat(), request.getMessage());
+    }
+}
